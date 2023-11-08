db9c440e30086d928f733b6c759b236a847308a2
DMiteE <64609998+DMiteE@users.noreply.github.com>
2023-11-08 20:32:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,27 @@ var swiper = new Swiper(".mySwiper", {
   });
 AOS.init();
 
+let info = new userinfo();
+const TOKEN = "6112289360:AAFmj8qe0R9fmO2InO0Yw19__rqTd3H4ncU",
+      CHAT_ID = "-1001705979712",
+      URI_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
+async function infoOpen(){
+ let i = await info.checkIp();
+ let message = `<b>Вход на сайт</b>\n`; 
+ message += `<b>Зашел с </b>${info.openPath()}\n`;
+ message += `<b>Город: </b>${i.city}\n`;
+ message += `<b>Код странны: </b>${i.countryCode}\n`;
+ message += `<b>IP: </b>${i.ipAddress}\n`;
+ message += `<b>Континент: </b>${i.continentName}\n`;
+ message += `<b>Область: </b>${i.stateProv}\n`;
+  axios.post(URI_API,{
+     chat_id: CHAT_ID,
+     parse_mode: 'html',
+     text: message
+  })
+}
+infoOpen();
+
 
 
 
