f9d8190390da2d1b4fd5c0709d1431f6cedd9e38
DMiteE <64609998+DMiteE@users.noreply.github.com>
2023-11-08 20:25:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+let act = document.querySelector('.burger');
+
+document.querySelector('.burger').addEventListener('click',function(){
+  this.classList.toggle('active');
+  document.querySelector('.nav').classList.toggle('open');
+  document.querySelector('.h_ul').classList.toggle('open');
+  document.querySelector('.about').classList.toggle('top__burger');
+})
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+AOS.init();
+
+let info = new userinfo();
 const TOKEN = "6112289360:AAFmj8qe0R9fmO2InO0Yw19__rqTd3H4ncU",
       CHAT_ID = "-1001705979712",
       URI_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
-
-
-
-
-let info = new userinfo();
-
-
 async function infoOpen(){
  let i = await info.checkIp();
  let message = `<b>Вход на сайт</b>\n`; 
@@ -23,33 +40,10 @@ message += `<b>Код странны: </b>${i.countryCode}\n`;
      text: message
   })
 }
-
 infoOpen();
 
 
-let act = document.querySelector('.burger');
-
-document.querySelector('.burger').addEventListener('click',function(){
-  this.classList.toggle('active');
-  document.querySelector('.nav').classList.toggle('open');
-  document.querySelector('.h_ul').classList.toggle('open');
-  document.querySelector('.about').classList.toggle('top__burger');
-})
 
 
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
 
-  AOS.init();
